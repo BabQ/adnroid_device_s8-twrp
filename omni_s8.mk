f27773048f0908bf2bfc2859bc88e9c8156f4541
babq <1153791778@qq.com>
2019-01-07 13:42:12 +0800

    s8 twrp 3.2.3

---
+++ omni_s8.mk
@@ -0,0 +1,9 @@
+# Inherit device configuration
+$(call inherit-product, device/s8/s8/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := s8
+PRODUCT_NAME := omni_s8
+PRODUCT_BRAND := s8
+PRODUCT_MANUFACTURER := s8
+PRODUCT_MODEL := s8
